4ceab5e10e39c38d150c352fbd68220e2d067580
08ppty <pemmarajupavan08@gmail.com>
2024-01-20 17:33:08 +0530

    Final code commit for merging

---
+++ ViewSavings_Digital_Bank_Pavan_code/test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"86431bae-a4c3-4a1c-9e3e-e88c961a7d6b","feature":"Testing banking website","scenario":"Testing Digital bank View savings","start":1705751618029,"group":1,"content":"","tags":"","end":1705751652505,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
